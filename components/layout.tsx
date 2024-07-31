8d9799f2c5d8756bb531828e21255ee70cc3b719
Reed Hazen <redhazen@gmail.cocm>
2024-07-31 10:42:31 -0700

    update netlify badge

--- components/layout.tsx
+++ components/layout.tsx
@@ -72,7 +72,9 @@ export default function Layout({ children, home }: { children: React.ReactNode; 
                         <EmailIcon fontSize="inherit" className={styles.socialIcon} color="primary" />
                     </a>
                 </div>
-                [![Netlify Status](https://api.netlify.com/api/v1/badges/de38faac-ff43-4250-a70a-4cdd92db7340/deploy-status)](https://app.netlify.com/sites/reedthazen-test/deploys)
+                <a href="https://app.netlify.com/sites/reedthazen-test/deploys">
+                    <img src="https://api.netlify.com/api/v1/badges/de38faac-ff43-4250-a70a-4cdd92db7340/deploy-status" alt="Netlify Status" />
+                </a>
             </footer>
         </div>
     );

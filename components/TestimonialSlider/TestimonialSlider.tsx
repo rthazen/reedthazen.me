910ae8580e7f2c1fb26acc21073e96cadacbcad1
Reed Hazen <redhazen@gmail.cocm>
2024-10-06 09:30:32 -0700

    use create component script to create initial Testimonial Slider component

---
+++ components/TestimonialSlider/TestimonialSlider.tsx
@@ -0,0 +1,11 @@
+import styles from './TestimonialSlider.module.css';
+
+const TestimonialSlider = () => {
+    return (
+        <div className={styles.TestimonialSlider}>
+            {/* Your component code */}
+        </div>
+    );
+};
+
+export default TestimonialSlider;
